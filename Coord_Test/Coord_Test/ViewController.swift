44b2f979072d170fda4fd17ab835b64254a99898
Suresh N <forsuresh@yandex.com>
2019-07-13 11:13:13 -0400

    project created

---
+++ Coord_Test/Coord_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coord_Test
+//
+//  Created by admin on 7/13/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
